d2e46fc5839a0b162165a7c6f84ff8b6b3981428
SkipKayhil <skipkayhil@gmail.com>
2017-07-11 03:24:45 -0400

    replaced service worker with call to unregister it

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 import { createStore } from 'redux';
 import reducer from './reducers'
 import Root from './components/Root';
-//import registerServiceWorker from './registerServiceWorker';
+import { unregister } from './registerServiceWorker';
 // import './index.css';
 
 const store = createStore(reducer);
@@ -14,4 +14,4 @@ render(
 );
 
 // TODO: I don't think we need this but I haven't done enough research
-//registerServiceWorker();
+unregister();
